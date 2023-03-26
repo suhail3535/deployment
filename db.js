90b860fdd94d749fa42de4f35c337804a4fbedaf
suhail3535 <khansohail015@gmail.com>
2023-03-26 17:15:37 +0530

    update

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connection = mongoose.connect(
-  "mongodb+srv://suhail:353035@cluster0.azpsxjp.mongodb.net/curdapp?retryWrites=true&w=majority"
+  "mongodb+srv://suhail:353035@cluster0.azpsxjp.mongodb.net/crudapp?retryWrites=true&w=majority"
 );
 
 module.exports = { connection };
